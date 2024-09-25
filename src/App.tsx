5db8c71e92845192b58ec3947de507985c81853a
Lucas Rose <lucas@dinewise.com>
2024-09-25 08:11:15 -0400

    update vite config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import 'tailwindcss/tailwind.css';
+import React from "react";
+import "./App.css";
+import "tailwindcss/tailwind.css";
 
 function App() {
   return (
@@ -13,11 +13,22 @@ function App() {
         <p className="small">
           Edit <code>src/App.jsx</code> and save to reload.
         </p>
-        <div className='gap-2'>
-          <span className='text-red-800'>Using the provided api we'd like to do the following:</span>
+        <div className="gap-2">
+          <span>Using the provided api we'd like to do the following:</span>
           <div>1. Fetch results & build a search component</div>
-          <div>2. Add the ability to save items from our list of results to different lists</div>
-          <div>3. Show our lists </div>
+          <div>
+            2. Add the ability to save items to different lists. Clicking a save
+            button should prompt list creation and naming if none exists. If a
+            list exists, it should show the list options to add to. (feel free
+            to persist in any storage you like)
+          </div>
+          <div>3. Clicking into a list shows each event with details</div>
+          <div>4. Delete and multi delete</div>
+
+          <div>
+            5. Other notes: feel free to reference any apis or library docs you
+            like, no copilot
+          </div>
         </div>
       </header>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import autoprefixerPlugin from "autoprefixer";
+import tailwindPlugin from "tailwindcss";
+import tailwindNestingPlugin from "tailwindcss/nesting";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindNestingPlugin, tailwindPlugin, autoprefixerPlugin],
+    },
+  },
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
   },
-})
+});
